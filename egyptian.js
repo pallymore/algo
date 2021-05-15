dd4c7aff9126b23221f96831cf5542bd7e98ecca
Yurui Zhang <yuruiology@gmail.com>
2021-05-14 22:30:29 -0400

    egyptian

---
+++ egyptian.js
@@ -0,0 +1,29 @@
+import test from "ava";
+
+const egyptian = ([a, b]) => {
+  if (b % a === 0) return [[1, b / a]];
+
+  const results = [];
+t  const newBase = (b + a - ((b + a) % a)) / a;
+  const egyptianRepresentation = [1, newBase];
+
+  results.push(egyptianRepresentation);
+
+  const restBase = newBase * b;
+  const restA = a * newBase - b;
+
+  const rest = egyptian([restA, restBase]);
+
+  return [...results, ...rest];
+};
+
+test("egyptian", (t) => {
+  const input = [4, 13];
+  const expected = [
+    [1, 4],
+    [1, 18],
+    [1, 468],
+  ];
+  const actual = egyptian(input);
+  t.deepEqual(expected, actual);
+});
