3f12f409bfd4f5a8ae0ecec2ddf2822c3d4dc981
Yurui Zhang <yuruiology@gmail.com>
2021-05-31 22:49:54 -0400

    get dom height

---
+++ DOMTreeHeight.js
@@ -0,0 +1,30 @@
+import test from "ava";
+
+test("DOM Tree Height", (t) => {
+  t.is(true, true, `this is tested in the browser`);
+});
+
+/*
+ *Height of a tree is the maximum depth from root node. Empty root node have a height of 0.
+
+  If given DOM tree, can you create a function to get the height of it?
+
+  For the DOM tree below, we have a height of 4.
+
+  <div>
+    <div>
+      <p>
+        <button>Hello</button>
+      </p>
+    </div>
+    <p>
+      <span>World!</span>
+    </p>
+  </div>
+ * */
+
+function getHeight(root) {
+  if (!root) return 0;
+  if (root.children.length === 0) return 1;
+  return 1 + Math.max(...Array.prototype.map.call(root.children, getHeight));
+}
